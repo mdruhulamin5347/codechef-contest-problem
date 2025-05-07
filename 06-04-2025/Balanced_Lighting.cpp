1e51c365c870f3824241eadf0efb715649ea8672
mdruhulamin5347 <mdruhulamin534793@gmail.com>
2025-05-07 22:47:22 +0600

    feat: add Balanced_Lighting.cpp solution for Codechef problem

---
+++ 06-04-2025/Balanced_Lighting.cpp
@@ -0,0 +1,38 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        int cnt_0 = 0, cnt_1 = 0, cnt_2 = 0;
+        for(int i=0; i<n; i++){
+            int x;
+            cin >> x;
+            if(x == 0){
+                cnt_0++;
+            }
+            else if(x == 1){
+                cnt_1++;
+            }
+            else{
+                cnt_2++;
+            }
+        }
+        if(n % 2 != 0){
+            cout << "No" << endl;
+        }
+        else if(abs(cnt_1 - cnt_2) <= cnt_0){
+            cout << "Yes" << endl;
+        }
+        else{
+            cout << "No" << endl;
+        }
+     
+    }
+    return 0;
+}
