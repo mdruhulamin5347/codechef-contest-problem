c63b567d6ee8144fa636f475f5675acee15a1b49
mdruhulamin5347 <mdruhulamin534793@gmail.com>
2025-05-28 23:47:32 +0600

    feat: add Subset_Sum_3.cpp solution for Codechef problem

---
+++ 28-05-2025/Subset_Sum_3.cpp
@@ -0,0 +1,90 @@
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+//     ios::sync_with_stdio(false);
+//     cin.tie(NULL);
+    
+//     int t;
+//     cin >> t;
+
+//     while(t--){
+//         int n;
+//         cin >> n;
+//         vector<int> v(n);
+//         for(int i=0; i<n; i++){
+//             cin >> v[i];
+//         }
+
+//         int l=0,r=0;
+//         int sum = 0;
+//         bool found = false;
+//         for(; l<n; ){
+//             while(r < n){
+//                 sum += v[r];
+//                 if(sum % 3 == 0){
+//                     found = true;
+//                 }
+//                 r++;
+//             }
+            
+//             sum -= v[l];
+//             l++;
+
+//         }
+        
+//         if(found){
+//             cout << "Yes" << endl;
+//         } else {
+//             cout << "No" << endl;
+//         }
+//     }
+    
+//     return 0;
+// }
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        for(int i = 0; i < n; i++){
+            cin >> v[i];
+        }
+
+        bool flag = false;
+
+        for(int i = 0; i < n; i++){
+            int sum = 0;
+            for(int j = i; j < n; j++){
+                sum += v[j];
+                if(sum % 3 == 0){
+                    flag = true;
+                    break;
+                }
+            }
+            if(flag) 
+                break;
+        }
+
+        if(flag) cout << "Yes" << endl;
+        else cout << "No" << endl;
+    }
+    
+    return 0;
+}
+
+
