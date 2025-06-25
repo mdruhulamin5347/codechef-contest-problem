4e23506651c7f1912457151ad4ebffa1b04a4233
mdruhulamin5347 <mdruhulamin534793@gmail.com>
2025-06-25 23:33:56 +0600

    feat: add Missing_Shoes.cpp solution for Codechef problem

---
+++ 25-06-2025/Missing_Shoes.cpp
@@ -0,0 +1,15 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int l,r;
+    cin >> l >> r;
+    cout << abs(l-r) ;
+    
+    return 0;
+}
